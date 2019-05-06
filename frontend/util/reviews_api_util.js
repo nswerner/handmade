73521c0351191bfb1e94e74068caae914ba7ee01
Nicholas Werner <nswerner@ucdavis.edu>
2019-05-06 15:01:23 -0400

    Create Review API Utils

---
+++ frontend/util/reviews_api_util.js
@@ -0,0 +1,40 @@
+export const createProductReview = (reviews) => {
+
+    return $.ajax({
+        method: "POST",
+        url: `/api/products/${reviews.product_id}/reviews`,
+        data: { reviews }
+    })
+}
+
+export const updateProductReview = (reviews) => {
+
+    return $.ajax({
+        method: "PATCH",
+        url: `/api/products/${reviews.product_id}/reviews/${reviews.id}`,
+        data: { reviews }
+    })
+}
+
+export const fetchReviews = (product_id) => {
+
+    return $.ajax({
+        method: "GET",
+        url: `/api/products/${product_id}/reviews`
+    })
+}
+
+export const fetchReview = (reviews) => {
+
+    return $.ajax({
+        method: "GET",
+        url: `/api/products/${reviews.product_id}/reviews/${reviews.id}`,
+    })
+}
+
+export const deleteReview = (reviews) => {
+    return $.ajax({
+        method: "DELETE",
+        url: `/api/products/${reviews.product_id}/reviews/${reviews.id}`,
+    })
+}
