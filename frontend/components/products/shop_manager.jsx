58ab909063cc5dd6335214cf3e340726738268d3
Nicholas Werner <nswerner@ucdavis.edu>
2019-06-20 16:44:41 -0400

    Add Gray Highlight to ShopManager Left Column Nav Selected

--- frontend/components/products/shop_manager.jsx
+++ frontend/components/products/shop_manager.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import ProductIndexItem from './product_index_items';
 
 class ShopManager extends React.Component {
     constructor(props) {
